298425fe5d04ef4de55705783356bafcbe74f933
Sebastian Villarreal <sebastianv09@gmail.com>
2018-03-25 15:23:43 -0400

    add and register updated task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,17 @@ module.exports = function(grunt) {
   grunt.initConfig({
     name: 'class',
     watch: {
-      files: ['*.js', '*.css']
+      files: ['*.js', '*.css'],
+      task: ['updated']
     }
   })
 
   // load watching into grunt
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('updated', () => {
+    grunt.log.writeln(`It updated again!`);
+  });
 
   // pull in name from initConfig above
   grunt.registerTask('default', () => {
